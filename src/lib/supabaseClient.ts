662d4d10865c9fc67b18046fcd8dc377a1290096
Namkha-yolo <oedzernamkha@gmail.com>
2025-06-14 22:22:09 -0400

    i think this scaffold will work

--- src/lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const URL  = import.meta.env.VITE_SUPABASE_URL!;
-const ANON = import.meta.env.VITE_SUPABASE_ANON!;
-
-export const supabase = createClient(URL, ANON);
